deb220bdfb03074555a7b9e9a3f3277576d70667
israel chigozirim <102589135+cocoyellow@users.noreply.github.com>
2022-07-24 20:10:48 +0000

    added a nav bar and made it responsive added minimal styling for the nav bar added javascript for the nav bar

--- app.mjs
+++ app.mjs
@@ -6,13 +6,26 @@ function startApp() {
     // pls remove the below and make some magic in here!
     console.log('make magic in here!');
   
+let nav = document.querySelector(".nav")
+let burger = document.querySelector(".burger i")
 let numberInput = document.querySelector("#numberInput");
 let submitButton = document.querySelector("#submitButton");
 let input_n = numberInput.value;
+//regex to check that input follows nigerian standard phone number protocol 
 let reg = /(?:^(\+234|234|0)((90)([1-9])|((70)([1-9]))|((80)([2-9]))|((81)([0-9])))([0-9]{7})$)/;
 let res = document.querySelector(".res");
 
+//event listeners
+burger.addEventListener("click", clickNavBar)
 submitButton.addEventListener("click",onClick);
+
+//to toggle nav bar
+function clickNavBar(){
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-xmark");
+    nav.classList.toggle("navActive");
+}
+
 
 function onClick(){
    //this helps us check that the input follows the guidelines for nigerian phone numbers
